726ecf985d4f2ab45dac4806c127fa5a964b6360
Yahirk40 <32582581+Yahirk40@users.noreply.github.com>
2017-10-18 17:49:04 -0500

    Ejemplo 2

--- main.c
+++ main.c
@@ -2,31 +2,30 @@
 #include <stdlib.h>
 
 int main(int argc, char *argv[])
-
 {
-    float i,im,t,ph,n;
+    int c ;
+    float m,n,s ;
+  c=1;
+  s=0;
+  m=0;
   
-    
-    
-    char nombre[50];
-    char a1[50];
-    char a2[50];
-    printf("Ingresa tu nombre con apellido:    ");
-    scanf("%s %s %s", &nombre, &a1, &a2);
-    
-    printf("¿Cuantas horas trabajaste en esta semana?   ");
-    scanf("%f",&t);
-    printf("¿Cuanto te pagan por hora?  ");
-    scanf("%f",&ph);
-    
-  
-    i= t*ph ;
-    im= 0.25*i ;
-    n= i-im;
-    printf("\n\n hola %s  %s  %s \n\n Tu ingreso semanal fue de %.1f  pesos \n  Tienes  un impuesto de %.1f pesos \n Tu sueldo neto es de %.1f pesos. \n " ,nombre,a1,a2,i,im,n); 
-    
-    
-    
+  label:
+  printf("Teclea un numero: ");
+  scanf("%f",&n);
+  if (n==0){
+           m=s/c;
+           printf("la media es %f ",m);
+           
+            
+            }
+            else{
+                 c=c+1;
+                 s=n+s;
+                 printf("la suma es %f ",s);
+                 goto label;
+                
+                 }
+                 
   
   system("PAUSE");	
   return 0;

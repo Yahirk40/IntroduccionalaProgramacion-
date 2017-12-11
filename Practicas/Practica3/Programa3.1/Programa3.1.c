93c3794922d92a62a86a9926545f12f0f70aa574
Yahirk40 <yahir.angelesv@gmail.com>
2017-12-11 16:20:31 -0600

    Edit

---
+++ Practicas/Practica3/Programa3.1/Programa3.1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+#include <stdlib.h>
+ 
+ int main(int argc, char *argv[])
+ {
+   float cent=5;
+   float alv;
+   alv=9.0/5.0*cent+32;
+   printf("%f\n",alv);
+   system("PAUSE");	
+   return 0;
+ }
